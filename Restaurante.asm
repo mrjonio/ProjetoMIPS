b9a7a0910de51e2da4ad0e807860a374d1970f7f
Carlos Antônio <carlosaajunior.jp@gmail.com>
2018-10-24 04:47:16 -0300

    1 nova exception adicionada (Nome muito grande), 3 novas fuções (acaoprato, chamarJanelaString, verificaçãoString) e inicio da funcao de cadastro de pratos

--- Restaurante.asm
+++ Restaurante.asm
@@ -1,12 +1,31 @@
 .data
 	#Menu Principal:
 	titulo: .asciiz "Escolha uma opção \n 1 - Clientes \n 2 - Pratos \n 3 - Funcionarios \n 4 - Mesa \n 5 - Pedidos \n"
+	
+	#Label: Exception: DadoInvalido
 	error: .asciiz "Dados inválidos!!!"
+	
+	#Label: Exception: NomeGrande
+	nomeGrandeErro: .asciiz "Nome muito grande!!"
+	
+	#SubMenus:
 	opcaoCliente: .asciiz "Escolha uma opção: \n 1 - Cadastrar um novo Cliente \n 2 - Remover um Cliente \n 3 - Atualizar informações de um cliente \n 4 - Visualizar informações de um cliente \n 5 - Fazer reserva para um cliente \n 6 - Retornar para o Menu Principal"
 	opcaoCardapio: .asciiz "Escolha uma opção: \n 1 - Adicionar novo prato ao cardápio \n 2 - Retirar um prato do cardápio \n 3 - Editar informações sobre um prato \n 4 - Vizualizar informações sobre um prato \n 5 - Visualizar Ranking de pratos mais vendidos \n 6 - Retornar para o Menu Principal"
 	opcaoFuncionario: .asciiz "Escolha uma opção: \n 1 - Contratar novo funcionário \n 2 - Demitir um funcionário \n 3 - Atualizar informações de um funcionário \n 4 - Visualizar informações de um funcionário \n 5 - Calcular folha de pagamento \n 6 - Retornar para o Menu Principal"
 	opcaoMesa: .asciiz "Escolha uma opção: \n 1 - Adicionar Mesa \n 2 - Retirar Mesa \n 3 - Mudar status da mesa \n 4 - Visualizar informações de uma Mesa \n 5 - Confirmar Reserva \n 6 - Limpar uma mesa \n 7 - Retornar para o Menu principal"
 	opcaoPedido: .asciiz "Escolha uma opção: \n 1 - Registrar um pedido \n 2 - Apagar(Cancelar) um pedido \n 3 - Refazer um pedido \n 4 - Visualizar um pedido \n 5 - Gerar lista de pedidos em determinado período de tempo \n 6 - Calcular Lucro dos pedidos em determinado período de tempo \n 7 - Completar pedido \n 8 - Retornar para o Menu Principal"
+	#fim dos subMenus.
+	
+	#Parametros (labels de pedido):
+	#Prato (Cadastro)
+	digiteNomePrato: .asciiz "Digite o nome do Prato: "
+	digitePrecoPrato: .asciiz "Digite o preço do Prato: "  
+	
+	#Parametros (Labels de armazenamento)
+	#Prato (Cadastro)
+	nomePrato: .space 20
+	
+	
 .text
 Main:
 #----------------------------------Menu Principal---------------------------------------------------------------------------------------------------
@@ -40,14 +59,32 @@ verificacao:  	bgt $t1, $a1, dadosInvalidos 	#Verifica se o usuario fez um opção
 		jr $ra				#Caso tudo esteja ok, o programa continua funcionando
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+#------------------------------------Verifica se a string colocada está correta--------------------------------------------------------------------
+verificacaoString:	beq $a1, 0, ok			#Se $a1 for 0, retorna pro fluxo do código
+			bgt $a1, 4, nomeGrande		#Se $a1 for 4, lança exception NomeGrande
+			beq $a1, 2, dadosInvalidos	#Se $a1 for 2, lança exception DadosInvalidos
+			beq $a1, 3, dadosInvalidos	#Se $a1 for 2, lança exception DadosInvalidos
+			jr $ra				#Se tudo estiver ok, volta pro fluxo do código
+
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 #----------------------------------Erro: Opção selecionada é inválida!!!----------------------------------------------------------------------------
-dadosInvalidos: 	addi $a1, $zero, 2	#Escolhendo tela de erro
-			la $a0, error		#Carregando a label que diz o erro
-			addi $t0, $zero, 55	#Escolhendo a tela de mensagens
-			sub $t2, $t2, $t2	#Colocando t2 para ter o valor 0 novamente
-			jal printf		#Chamando o print [ printf( error) ]
-			j Main			#Fim do tratamento da exceção
+dadosInvalidos: 	addi $a1, $zero, 2		#Escolhendo tela de erro
+			la $a0, error			#Carregando a label que diz o erro
+			addi $t0, $zero, 55		#Escolhendo a tela de mensagens
+			sub $t2, $t2, $t2		#Colocando t2 para ter o valor 0 novamente
+			jal printf			#Chamando o print [ printf( error) ]
+			j Main				#Fim do tratamento da exceção
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#---------------------------------------------Erro: Nome muito grande!!!--------------------------------------------------------------------------
+nomeGrande:		addi $a1, $zero, 2		#Escolhendo tela de erro
+			la $a0, nomeGrandeErro		#Carregando a label que diz o erro
+			addi $t0, $zero, 55		#Escolhendo a tela de mensagens
+			sub $t2, $t2, $t2		#Colocando t2 para ter o valor 0 novamente
+			jal printf			#Chamando o print [ printf( error) ]
+			j Main				#Fim do tratamento da exceção
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 #--------------------------------Verificar menu escolhido (verifica qual foi a escolha de sub menu) ------------------------------------------------
 subMenu:	beq $a0, 1, menuCliente		#Menu do cliente foi escolhido
@@ -74,6 +111,8 @@ menuPrato: 	la $t0, opcaoCardapio	#Carrega o menu dos pratos
 		addi $t1, $t1, 0	#Parâmetro pra saber se a opção escolhido é maior que 0
 		addi $t2, $zero, 6	#Parâmetro pra saber se a opção escolhida é menor ou igual a 6
 		jal verificacao		#Função que verifica se a opção escolhida é um número entre 1 e 6 [ verificacao(0, 6) ]
+		add $t3, $t3, $a0	#Adicionando a opção escolhida para um registrador temporario
+		jal acaoPrato		#Chamando a função de verificação de escolha
 		j Main			#Fim das operações com o cardapio
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
@@ -103,5 +142,71 @@ menuPedidos: 	la $t0, opcaoPedido	#Carrega o menu de Pedidos
 		jal verificacao		#Função que verifica se a opção escolhida é um número entre 1 e 8 [ verificacao(0, 8) ]
 		j Main			#Fim das operações com os Pedidos
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#-------------------------------------------------Chamar janela de string--------------------------------------------------------------------------
+chamarJanelaString: 	addi $v0, $zero, 54
+			la $a0, ($t2)
+			la $a1, ($t3)
+			add $a2, $zero, $t4
+			syscall
+			jr $ra
+
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#-------------------------------------------------Cadastrar Prato----------------------------------------------------------------------------------
+acaoPrato:	beq $t3, 1, funcaoCadastrarPrato	#Chamada da função de cadastro de prato escolhida
+		beq $t3, 2, funcaoRemoverPrato		#Chamada da funcao de remoção de pratos escolhida
+		beq $t3, 3, funcaoEditarPrato		#Chamada da função de edição de pratos escolhida
+		beq $t3, 4, funcaoVisualizarPrato	#Chamada da função de uso/visualização de pratos escolhida
+		beq $t3, 5, funcaoRankingPratos        #Chamada da função de ranking de Pratos escolhida
+		beq $t3, 6, retornaMain			#Retornar para menu principal escolhido
+			
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+#-------------------------------------------------Cadastrar Prato----------------------------------------------------------------------------------
+funcaoCadastrarPrato: 	la $t2, digiteNomePrato
+			la $t3, nomePrato
+			addi $t4, $zero, 10
+			jal chamarJanelaString
+			jal verificacaoString
+			la $a0, digitePrecoPrato
+			addi $v0, $zero, 53
+			syscall
+			bgt $a1, 0, dadosInvalidos
+			j exit
+			
+			
+			
+			
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#-------------------------------------------------Remover Prato----------------------------------------------------------------------------------
+funcaoRemoverPrato: 
+			
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#-------------------------------------------------Editar Prato----------------------------------------------------------------------------------
+funcaoEditarPrato: 
+			
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#-------------------------------------------------Visualizar Prato----------------------------------------------------------------------------------
+funcaoVisualizarPrato: 
+			
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#-------------------------------------------------Ranking Pratos----------------------------------------------------------------------------------
+funcaoRankingPratos: 
+			
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#-------------------------------------------------Voltar pro main menu----------------------------------------------------------------------------------
+retornaMain: 
+			
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+ok: jr $ra
+
 exit: nop
 	
